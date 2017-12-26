49f88bd547dd71b8beb2420cee88051011965589
jefelewis <jeffylew77@gmail.com>
2017-12-25 22:31:03 -0700

    Added comments to layout the HTML file and added a base CSS file

--- data_generator.js
+++ data_generator.js
@@ -3,7 +3,11 @@
  * You can safely leave this file untouched, and confine your changes to index.html.
  */
 
-// set up data structures
+// Set up data structures
+// users: An array of strings, which are the usernames that you're following
+// streams: An object with two properties, users and home
+  // streams.home: An array of all tweets from all the users you're following
+  // streams.users is an object with properties for each user (streams.users.shawndrost has all of shawndrost's tweets)
 window.streams = {};
 streams.home = [];
 streams.users = {};
@@ -13,20 +17,20 @@ streams.users.mracus = [];
 streams.users.douglascalhoun = [];
 window.users = Object.keys(streams.users);
 
-// utility function for adding tweets to our data structures
+// Utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
   var username = newTweet.user;
   streams.users[username].push(newTweet);
   streams.home.push(newTweet);
 };
 
-// utility function
+// Utility function
 var randomElement = function(array){
   var randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 };
 
-// random tweet generator
+// Random tweet generator
 var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
 var verbs = ['downloaded', 'interfaced', 'deployed', 'developed', 'built', 'invented', 'experienced', 'navigated', 'aided', 'enjoyed', 'engineered', 'installed', 'debugged', 'delegated', 'automated', 'formulated', 'systematized', 'overhauled', 'computed'];
 var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
@@ -37,7 +41,7 @@ var randomMessage = function(){
   return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
 };
 
-// generate random tweets on a random schedule
+// Generate random tweets on a random schedule
 var generateRandomTweet = function(){
   var tweet = {};
   tweet.user = randomElement(users);
@@ -56,7 +60,7 @@ var scheduleNextTweet = function(){
 };
 scheduleNextTweet();
 
-// utility function for letting students add "write a tweet" functionality
+// Utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
 var writeTweet = function(message){
   if(!visitor){
